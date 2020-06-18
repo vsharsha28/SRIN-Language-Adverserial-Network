580b712ea62e094c0296cd266e09b9046056fbbf
Himanshu <himanshu@ee.iitr.ac.in>
2020-06-18 09:56:22 +0530

    Add files via upload

---
+++ code/train.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env ipython
+
+import tensorflow as tf
+import json
+
+import os
+from pathlib import Path
+os.chdir(os.path.dirname(__file__))
+
+def decode(infile, n=-1):
+	assert os.path.isfile(infile), str(os.getcwd() / infile) + " doesn't exist"
+	with open(infile, 'r') as infile:
+		if n == -1:
+			while(not infile.)
+			line = infile.readline()
+			dic = json.loads(line)
+			yield(tf.convert_to_tensor([dic['review_body'], dic['stars']]))
+		else
+
+
+infile = Path('Amazon Reviews/test/dataset_en_test.json')
+assert os.path.isfile(infile), str(os.getcwd() / infile) + " doesn't exist"
+with open(infile, 'r') as infile:
+	line = infile.readline()
+	dic = json.loads(line)
+	print(tf.convert_to_tensor([dic['review_body'], dic['stars']]))
