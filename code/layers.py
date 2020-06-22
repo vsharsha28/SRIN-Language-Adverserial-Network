4a6985bad4e9e85f4c5b6bc858a6c9b9d93bc260
Himanshu <himanshu@ee.iitr.ac.in>
2020-06-22 20:44:35 +0530

    Update layers.py

--- code/layers.py
+++ code/layers.py
@@ -3,14 +3,25 @@
 import tensorflow as tf
 from tensorflow import keras, layers, models
 
-class Average(tf.Module):
-	def __init__(self, word_emb):
+class Average(keras.Model):
+	def __init__(self, emb_layer):
 		super(Average, self).__init__()
-		self.word_emb = word_emb
+		self.emb_layer = emb_layer
 
-	def forward(self, input):
+	def call(self, input_seq_batch : tf.Tensor):
 		"""
 		input: (data, lengths): (IntTensor(batch_size, max_sent_len), IntTensor(batch_size))
 		"""
-		data, lengths = input
-		embeds = self.word_emb(data)
+		#embs = self.emb_layer(input) #, length
+		#X = tf.reduce_sum(embs, axis=1)
+		#lengths (pass as parameter) = tf.resape(lengths, (-1, 1)).broadcast_to(X.shape)
+		embeddings = self.emb_layer(input_seq_batch)
+		self.W = tf.reduce_mean(tf.cast(embeddings, dtype=tf.float32), axis=1)
+		return self.W
+
+class Summing(layers.Layer):
+
+
+
+
+class DotAttention(layers.Layer):
